33ce807f454a30cbfb1a88765290c1021e670d36
Joshua Lynch <135474581+Joshua-Lynch@users.noreply.github.com>
2025-09-16 21:01:03 -0400

    BB-11
    Implemented core game logic

--- BoyardeeBois-TicTacToe/BoyardeeBois-TicTacToe.cpp
+++ BoyardeeBois-TicTacToe/BoyardeeBois-TicTacToe.cpp
@@ -5,41 +5,226 @@ Authors: Joshua Klenk, Joshua Lynch, Erwin Jonson,
          Damian Mercedes, Daniel Brooksbank
 
 Date: 9.12.25         
-Version 1.0
+Version 2.0
 */
 
 #include <iostream>
+#include <limits>
 using namespace std;
+
+// making the board into a 2D array with a grid style to replace digits easily -JoshL
+char board[3][3] =
+{
+    {' ', ' ', ' '},
+    {' ', ' ', ' '},
+    {' ', ' ', ' '}
+};
+
+void TicTacToeBoard() //Had to remove a lot of the #'s because it made screen cluttered
+{
+    cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << "\n"
+        << "---+---+---\n"
+        << " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << "\n"
+        << "---+---+---\n"
+        << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << "\n\n"
+        << " \n"
+        << " \n";
+    cout << "#########################################################" << endl;
+
+
+}
+
+char checkWinner3by3(char board[][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[i][0] == board[i][1] && board[i][0] == board[i][2])
+            return board[i][0];
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[0][i] == board[1][i] && board[0][i] == board[2][i])
+            return board[0][i];
+    }
+
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+    {
+        return board[0][0];
+    }
+
+
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+    {
+        return board[0][2];
+    }
+
+    return ' ';
+}
+
+bool getPlayerMove(char currentPlayer)
+{
+    int choice;
+    while (true)
+    {
+
+        // Handles invalid input 
+        if (!(cin >> choice))
+        {
+
+            cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << endl;
+            TicTacToeBoard();
+            cout << "Invalid input. Please enter a number between 1 and 9.\n" << endl;
+
+            continue;
+        }
+
+
+        if ((choice < 1 || choice > 9))
+        {
+
+            cout << endl;
+            TicTacToeBoard();
+            cout << "Invalid input. Please enter a number between 1 and 9.\n" << endl;
+            continue;
+        }
+        int row = (choice - 1) / 3;
+        int col = (choice - 1) % 3;
+
+        if (board[row][col] == 'X' || board[row][col] == 'O' || board[row][col] == 'x' || board[row][col] == 'o')
+        {
+            cout << "This space is taken, Choose another!" << endl;
+            TicTacToeBoard();
+            continue;
+        }
+        board[(choice - 1) / 3][(choice - 1) % 3] = currentPlayer;
+        return true;
+    }
+
+
+
+}
+
+
+
 
 int main()
 {
 
-		cout
-		<< "##############################################################################\n"
-		<< "##############################################################################\n"
-		<< "#####################      Welcome to Tic Tac Toe!      ######################\n"
-		<< "##############################################################################\n"
-		<< "##############################################################################\n"
-		<< " \n"
-		<< " \n"
+    char answer;
+    do
+    {
+        system("cls");
+        cout << "\n##### Welcome to Boyardee Bois' Tic Tac Toe! ######\n\n" << endl;
 
-		<< "                              |           |           \n"
-		<< "                        1     |     2     |     3     \n"
-		<< "                              |           |           \n"
-		<< "                   -----------+-----------+-----------\n"
-		<< "                              |           |           \n"
-		<< "                        4     |     5     |     6     \n"
-		<< "                              |           |           \n"
-		<< "                   -----------+-----------+-----------\n"
-		<< "                              |           |           \n"
-		<< "                        7     |     8     |     9     \n"
-		<< "                              |           |           \n"
+        // Resets board at the start of each game
+        char blankSpace = ' ';
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++)
+            {
+                board[i][j] = blankSpace;
+            }
+        }
 
-		<< " \n"
-		<< " \n"
-		<< "##############################################################################\n"
-		<< "##############################################################################\n"
-		<< "##############################################################################\n"
-		<< "##############################################################################\n"
-		<< "##############################################################################";
+        char player1, player2;
+
+
+        cout << "Player 1, choose your marker (X/O): ";
+        cin >> player1;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        while (player1 != 'X' && player1 != 'x' && player1 != 'O' && player1 != 'o')
+        {
+            cout << "Invalid input" << endl;
+
+            cout << "Player 1, choose your marker (X/O): " << endl;
+
+
+            if (!(cin >> player1))
+            {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                continue;
+            }
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+
+
+        if (player1 == 'X' || player1 == 'x')
+        {
+            player1 = 'X';
+            player2 = 'O';
+
+        }
+        else
+        {
+            player1 = 'O';
+            player2 = 'X';
+
+        }
+
+
+
+
+        // Main game loop (max 9 turns)
+        for (int turn = 0; turn < 9; turn++)
+        {
+            TicTacToeBoard();
+            cout << endl << endl;
+
+
+            char currentPlayer{};
+            int currentPlayerNum{};
+            cout << "Player " << currentPlayer + 1 << ", enter your choice (1-9): ";
+
+
+            if (turn % 2 == 0)
+            {
+                currentPlayer = player1;
+                currentPlayerNum = 1;
+            }
+            else
+            {
+                currentPlayer = player2;
+                currentPlayerNum = 2;
+            }
+
+            bool validMove = false;
+            while (!validMove)
+            {
+                validMove = getPlayerMove(currentPlayer);
+            }
+
+            // Check for a winner
+            char winner = checkWinner3by3(board);
+            if (winner == 'X' || winner == 'O')
+            {
+                TicTacToeBoard();
+                cout << "Player " << currentPlayerNum << " (" << winner << ") wins!\n";
+                break;
+            }
+
+            // Draw if all 9 turns are used
+            if (turn == 8) {
+                TicTacToeBoard();
+                cout << "It's a draw!\n";
+            }
+        }
+
+        cout << "Do you want to play again? (Y/N): ";
+        cin >> answer;
+
+        while (answer != 'Y' && answer != 'y' && answer != 'n' && answer != 'N')
+        {
+            cout << "Invalid input" << endl;
+            cout << "Do you want to play again? (Y/N): " << endl;
+            cin >> answer;
+
+        }
+
+    } while (answer == 'Y' || answer == 'y');
+
+    return 0;
 }
